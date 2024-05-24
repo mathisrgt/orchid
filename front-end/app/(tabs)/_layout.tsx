41d8d7a1d3140029c9598ac5ef5621fb2cf0b2b8
MarcBTHT <marc.berthol29@gmail.com>
2024-05-24 16:08:36 +0200

    feat: :sparkles: Sui ZkLogin integration

--- front-end/app/(tabs)/_layout.tsx
+++ front-end/app/(tabs)/_layout.tsx
@@ -61,6 +61,13 @@ export default function TabLayout() {
           tabBarIcon: ({ color }) => <TabBarIcon name="bank" color={color} />,
         }}
       />
+      <Tabs.Screen
+        name="zkLogin"
+        options={{
+          title: 'Login',
+          tabBarIcon: ({ color }) => <TabBarIcon name="user" color={color} />,
+        }}
+      />
     </Tabs>
   );
 }
