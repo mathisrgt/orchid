b3a41aa5fc3916c878e1b4247e0dc51a4fc46344
mathis sergent <sergent.mathis@gmail.com>
2024-05-29 15:38:01 +0200

    zkLogin import vercel testing

--- front-end/app/page.tsx
+++ front-end/app/page.tsx
@@ -3,11 +3,13 @@
 import Image from "next/image";
 
 import { ConnectButton } from "@mysten/dapp-kit";
+import ZkLoginButton from "@/components/zkLoginButton";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1>Welcome to Orchid</h1>
+      <ZkLoginButton />
       <ConnectButton />
     </main>
   );
